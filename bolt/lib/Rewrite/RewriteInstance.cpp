0c7d47d7fdb3fea0234d0f3a8945920c752f3df0
Gergely Balint <gergely.balint@arm.com>
2025-09-11 10:07:47 +0000

    [BOLT] Rename pac-ret feature flag
    - new name is update-branch-protection, which can be extended later to
      include a "strict mode", and to cover BTI rewriting.

--- bolt/lib/Rewrite/RewriteInstance.cpp
+++ bolt/lib/Rewrite/RewriteInstance.cpp
@@ -3494,10 +3494,10 @@ void RewriteInstance::disassembleFunctions() {
     // Check if fillCFIInfoFor removed any OpNegateRAState CFIs from the
     // function.
     if (Function.containedNegateRAState()) {
-      if (opts::DisallowPacret) {
-        BC->errs() << "BOLT-ERROR: --disallow-pacret flag was used, but "
-                   << Function.getPrintName()
-                   << " contains .cfi-negate-ra-state.\n";
+      if (!opts::UpdateBranchProtection) {
+        BC->errs()
+            << "BOLT-ERROR: --update-branch-protection is set to false, but "
+            << Function.getPrintName() << " contains .cfi-negate-ra-state.\n";
         exit(1);
       }
     }
